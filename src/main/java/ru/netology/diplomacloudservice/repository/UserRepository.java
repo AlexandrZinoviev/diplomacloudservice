b649f129386fd7fe7de8b89b064c5c8855977e79
AlexandrZinoviev <z.alexu@mail.ru>
2024-02-25 17:14:13 +0300

    Доработка дипломного проекта

--- src/main/java/ru/netology/diplomacloudservice/repository/UserRepository.java
+++ src/main/java/ru/netology/diplomacloudservice/repository/UserRepository.java
@@ -2,7 +2,7 @@ package ru.netology.diplomacloudservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.werest.diplomacloudservice.entity.User;
+import ru.netology.diplomacloudservice.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
